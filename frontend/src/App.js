8978cb7c23453b9db5c48136861e94c1c4cd301c
Евгений Бакаев <bak.evsha@gmail.com>
2023-05-30 22:46:20 +0300

    init frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { FileUploader } from "./file-uploader";
+import Canvas from "./components/Canvas";
+
+export default function App() {
+  return (
+    <div className="page">
+      <Canvas />
+    </div>
+  );
+}
---
+++ frontend/src/components/Canvas.js
@@ -0,0 +1,9 @@
+import './Canvas.css'
+
+export default function Canvas(methods: any) {
+  return (
+    <div className="canvas">
+
+    </div>
+  )
+}
